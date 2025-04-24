4b758ab6a7cd4184684fc16e3405eb50e96a0a6c
alechicken <alessio.gallina@capellinisauro.edu.it>
2025-04-24 07:33:19 +0000

    Stampa data di nascita

--- hellogallina.java
+++ hellogallina.java
@@ -3,5 +3,6 @@ public class hellogallina {
         System.out.println("Hello Alessio");
         System.out.println("Hello Alessio");
         System.out.println("Hello Alessio");//stampa 3 volte nome
+        System.out.println("23 Gennaio 2008");//stampa data di nascita
     }
 }
