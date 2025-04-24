2932b4f2748feaa65e247a04844315a1e57aa761
alechicken <alessio.gallina@capellinisauro.edu.it>
2025-04-24 07:28:56 +0000

    Stampa nome 3 volte

--- hellogallina.java
+++ hellogallina.java
@@ -1,5 +1,7 @@
 public class hellogallina {
         public static void main(String[] args){
         System.out.println("Hello Alessio");
+        System.out.println("Hello Alessio");
+        System.out.println("Hello Alessio");//stampa 3 volte nome
     }
 }
