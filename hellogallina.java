ab32b38dd9e8e9bc78773c20a15a17c93fd1a8aa
alechicken <alessio.gallina@capellinisauro.edu.it>
2025-05-08 06:59:51 +0000

    Aggiunta stampa da Array Statico

--- hellogallina.java
+++ hellogallina.java
@@ -1,5 +1,17 @@
 public class hellogallina {
         public static void main(String[] args){
+        String[] arrayNome = new String[7];
+        arrayNome[0] = "A";
+        arrayNome[1] = "l";
+        arrayNome[2] = "e";
+        arrayNome[3] = "s"; 
+        arrayNome[4] = "s";
+        arrayNome[5] = "i";
+        arrayNome[6] = "o";
+        for (int i = 0; i < 7; i++){
+            System.out.println(arrayNome[i]);
+        }
+        
         System.out.println("Hello Alessio");
         System.out.println("Hello Alessio");
         System.out.println("Hello Alessio");//stampa 3 volte nome
